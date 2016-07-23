775c9d64da1dfa549b48ed7d0399da369e6921c3
rakesh <rakesh.gandu15@gmail.com>
2016-07-23 18:25:48 -0500

    removed the file

--- _3ClassesAndObjects/src/_1ClassesAbstraction.java
+++
@@ -1,9 +0,0 @@
-public class _1ClassesAbstraction {
-
-	public static void main(String[] args) {
-		Calculator cal=new Calculator();
-		System.out.println(cal.sum(5,6));
-		System.out.println(cal.subtract(5,6));
-		
-	}
-}
